82ebfd810096222023baab17b1860b3ec849a5c4
hemanshi1036 <hemanshisolanki2004@gmail.com>
2024-11-10 22:46:02 +0530

    error solve redusers lakhvu

--- shopping-cart/src/redux/Slices/CartSlice.js
+++ shopping-cart/src/redux/Slices/CartSlice.js
@@ -1,39 +1,41 @@
-// import { createSlice } from "@reduxjs/toolkit";
-
-// export const CartSlice = createSlice({
-//     name:"cart",
-//     initialState: [],
-//     reducer:{
-//         add:(state,action) => {
-//             state.push(action.payload);
-
-//         },
-//         remove:(state,action) => {
-//             return state.filter((item) => item.id !== action.payload)
-//         }
-//     }
-// })
-
-// export const {add, remove} = CartSlice.actions;
-// export default CartSlice.reducer; 
-
-
-
 import { createSlice } from "@reduxjs/toolkit";
-
 
 export const CartSlice = createSlice({
     name:"cart",
-    initialState:[],
+    initialState: [],
+    //ahiya reducers lakhvu baki error aavse
+    
     reducers:{
         add:(state,action) => {
             state.push(action.payload);
+
         },
         remove:(state,action) => {
-            return state.filter((item) => item.id !== action.payload);
-        },
+            return state.filter((item) => item.id !== action.payload)
+        }
     }
-});
+})
 
 export const {add, remove} = CartSlice.actions;
-export default CartSlice.reducer;
+export default CartSlice.reducer; 
+
+
+
+// import { createSlice } from "@reduxjs/toolkit";
+
+
+// export const CartSlice = createSlice({
+//     name:"cart",
+//     initialState:[],
+//     reducers:{
+//         add:(state,action) => {
+//             state.push(action.payload);
+//         },
+//         remove:(state,action) => {
+//             return state.filter((item) => item.id !== action.payload);
+//         },
+//     }
+// });
+
+// export const {add, remove} = CartSlice.actions;
+// export default CartSlice.reducer;
